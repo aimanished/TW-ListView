014a3646351282aabf56ceb8ab13ff66b1cf4ebc
Aiman Danish <aimanished@gmail.com>
2018-04-24 12:52:16 +0800

    try only

--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -1,12 +1,35 @@
 package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ModuleAdapter {
+import java.util.ArrayList;
+
+public class ModuleAdapter extends ArrayAdapter<Modules> {
 
     private Context context;
+    private ArrayList<Modules> modules;
+    private TextView tv;
+    private ImageView imageView;
 
 
+    public ModuleAdapter(Context context,int resource , ArrayList<Modules> objects){
+        super(context,resource,objects);
+        modules = objects;
+        this.context = context;
+    }
 
+    @Override
+    public View getView(int position, View converView, ViewGroup parent){
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.row,parent,false);
+        tv = (TextView)rowView.findViewById(R.id.textView);
+        
+    }
 
 }
